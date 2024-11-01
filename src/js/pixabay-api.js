f3fc8a76cfcce756b488d6643bf1a164138d1800
Marina.S. <marina.sevostian@gmail.com>
2024-11-01 05:42:36 +0200

    add pagination and search for the end of the collection

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,26 +3,57 @@ import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-export async function fetchImages(inputValue) {
-  return await fetch(
-    `https://pixabay.com/api/?key=46729875-7729b8e358007a47de817f6d1&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(async response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return await response.data;
-    })
-    .catch(error => {
+export default class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.per_page = 15;
+  }
+  async fetchImages() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '46729875-7729b8e358007a47de817f6d1',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.per_page}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
+      const data = response.data;
+      this.incrementPage();
+      return data;
+    } catch (error) {
       loader.classList.replace('loader', 'hide');
+      loadMoreBtn.classList.replace('load-more', 'hide');
       iziToast.error({
         title: 'Error',
         message: `Error! Sorry, something went wrong. This is an error!`,
         color: '#ef4040',
         close: false,
       });
-    });
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { fetchImages } from './js/pixabay-api';
+import { NewApiService } from './js/pixabay-api';
 import { renderImages } from './js/render-functions';
 
 import SimpleLightbox from 'simplelightbox';
@@ -15,38 +15,35 @@ let searchFormEl = document.querySelector('.search-form');
 let inputForm = document.querySelector('.search-form-input');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 let loader = document.querySelector('.loader');
+let loadMoreBtn = document.querySelector('.load-more');
 
 loader.classList.replace('loader', 'hide');
+loadMoreBtn.classList.replace('load-more', 'hide');
+
+let isShown = 0;
+const newsApiService = new NewApiService();
 
 searchFormEl.addEventListener('submit', async e => {
   e.preventDefault();
-  const inputValue = inputForm.value.trim();
   cleanHtml();
+  newsApiService.query = inputForm.value.trim();
   loader.classList.replace('hide', 'loader');
-  if (inputValue !== '') {
-    await fetchImages(inputValue)
-      .then(images => {
-        if (images.hits.length === 0) {
-          iziToast.error({
-            title: 'Error',
-            message: `Sorry, there are no images matching your search query. Please try again!`,
-            color: '#ef4040',
-            close: false,
-          });
-        }
-        renderImages(images.hits);
-        gallerySimpleLightbox.refresh();
-        loader.classList.replace('loader', 'hide');
-      })
-      .catch(error => {
-        loader.classList.replace('loader', 'hide');
-        iziToast.error({
-          title: 'Error',
-          message: `Error! Sorry, something went wrong. This is an error!`,
-          color: '#ef4040',
-          close: false,
-        });
-      });
+  loadMoreBtn.classList.replace('load-more', 'hide');
+  newsApiService.resetPage();
+  if (newsApiService.query !== '') {
+    isShown = 0;
+    fetchImages();
+    renderImages(hits);
+    // catch (error) {
+    //   loader.classList.replace('loader', 'hide');
+    //   loadMoreBtn.classList.replace('load-more', 'hide');
+    //   iziToast.error({
+    //     title: 'Error',
+    //     message: `Error! Sorry, something went wrong. This is an error!`,
+    //     color: '#ef4040',
+    //     close: false,
+    //   });
+    // }
     searchFormEl.reset();
   } else {
     loader.classList.replace('loader', 'hide');
@@ -56,10 +53,12 @@ searchFormEl.addEventListener('submit', async e => {
       color: '#ffa000',
       close: false,
     });
+    return;
   }
 });
 
 new SimpleLightbox('.gallery a', {
+  captions: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
@@ -67,14 +66,37 @@ function cleanHtml() {
   gallery.innerHTML = '';
 }
 
-// let loader = document.querySelector('.loader');
+async function fetchImages() {
+  loadMoreBtn.classList.replace('load-more', 'hide');
 
-// window.addEventListener('load', () => {
-//   loader.classList.add('.visually-hidden');
-//   setTimeout(() => {
-//     loader.remove();
-//   }, 600);
-// });
-// let loader = document.querySelector('.loader');
-// loader.classList.remove('.visually-hidden');
-// loader.classList.add('.visually-hidden');
+  const result = await newsApiService.fetchImages();
+  const { hits, total } = result;
+  isShown += hits.length;
+
+  if (!hits.length) {
+    iziToast.error({
+      title: 'Error',
+      message: `Sorry, there are no images matching your search query. Please try again!`,
+      color: '#ef4040',
+      close: false,
+    });
+    loadMoreBtn.classList.replace('load-more', 'hide');
+    return;
+  }
+  renderImages(hits);
+  isShown += hits.length;
+  gallerySimpleLightbox.refresh();
+  loader.classList.replace('loader', 'hide');
+  if (isShown < total) {
+    loadMoreBtn.classList.replace('hide', 'load-more');
+  }
+  if (isShown >= total) {
+    loadMoreBtn.classList.replace('load-more', 'hide');
+    iziToast.error({
+      title: 'Error',
+      message: `We're sorry, but you've reached the end of search results.`,
+      color: '#ef4040',
+      close: false,
+    });
+  }
+}
